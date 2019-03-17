4e39db0b8a9ae67eaf680fb82bff6b2b77876dbe
Canicular <37901967+Min1307@users.noreply.github.com>
2019-03-17 10:26:40 +0800

    Create 136. Single Number.cpp

---
+++ 136. Single Number.cpp
@@ -0,0 +1,13 @@
+//136. Single Number
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int length = nums.size();
+        int result = 0 ;
+        for (int i=0; i<length; i++)
+        {
+            result ^= nums[i];
+        }
+        return result;
+    }
+};
